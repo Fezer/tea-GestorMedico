ff1549f4139250d0e16722be653f15e198028b8b
Nickolas Ukasinski <nickolas.ukasinski@tuntscorp.com>
2022-03-03 21:23:31 -0300

    main router implementes

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const appointmentRouter = require('./appointmentRouter.js')
+const pacientRouter = require('./pacientRouter.js')
+const physicianRouter = require('./physicianRouter.js')
 const routes = express.Router();
 
+router.get('/', (req, res)=>{
+    res.send('Its working');
+});
 
+router.use('/appointment', appointmentRouter);
+router.use('/pacient', pacientRouter);
+router.use('/physician', physicianRouter);
 
-module.exports = routes;
+module.exports = router;
