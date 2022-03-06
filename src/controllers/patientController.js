c5b37f9deea888c3aa462b80179187c896205d37
unknown <gabrielwo314@gmail.com>
2022-03-05 22:13:58 -0300

    createPatient

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -3,18 +3,48 @@ const sequelize = require("sequelize");
 
 module.exports = {
   async listAllPatient(req, res) {
-    console.log("listAllPatient");
     const patients = await Patient.findAll({
       order: [["name", "ASC"]],
     }).catch((error) => {
       return res.status(500).json({ msg: "Falha na conexão." });
     });
     if (!patients || patients == undefined){
-      return res.status(404).json({ msg: "Não foi possivel encontrar médicos." });
+      return res.status(404).json({ msg: "Não foi possivel encontrar pacientes." });
     }else{
       return res.status(200).json({ patients });
     }
   },
 
-  
+  async createPatient(req, res) {
+
+    const { name, email, phone } = req.body;
+    if(!name || !email || !phone)
+      return res.status(400).json({ msg: "Dados do paciente não foram preenchidos."});
+
+    const isPatientNew = await Patient.findOne({
+      where: { email },
+    }).catch((error) => {
+      return res.status(500).json({ msg: "Falha na conexão." });
+    });
+
+    if(isPatientNew)
+      return res.status(403).json({ msg: "Paciente já cadastrado."});
+    else {
+      const patient = await Patient.create({
+        name,
+        email,
+        phone,
+      }).catch((error) => {
+        return res.status(500).json({ msg: "Falha na conexão." });
+      });
+      
+      if(patient)
+        return res.status(201).json({ msg: "Novo paciente cadastrado com sucesso." });
+      else
+        return res.status(404).json({ msg: "Houve um erro na cadastro do paciente." });
+    }
+    
+  },
+
+
 }
--- src/routes/patientRouter.js
+++ src/routes/patientRouter.js
@@ -3,5 +3,6 @@ const patientRouter = express.Router();
 const patientController = require("../controllers/patientController");
 
 patientRouter.get("/listPatients", patientController.listAllPatient);
+patientRouter.post("/createPatient", patientController.createPatient);
 
 module.exports = patientRouter;
